13f5d1f03b8cd4257ca83813388126a778afd2b2
Taishun Kusaka <31302181+taikusa@users.noreply.github.com>
2023-11-22 10:11:59 +0900

    トップにナビゲーションバーとロゴを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.png";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="min-h-[6rem] min-w-[18rem] max-w-8xl p-4">
+      <div className="navbar shadow-xl rounded-box bg-base-100">
+        <img className="p-[.4rem]" src={logo} alt="logo" width="48"/>
+      </div>
+      </div>
     </div>
   );
 }
