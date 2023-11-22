0e2a79093fd509362b5698dadd1c09bd432ea0b1
Taishun Kusaka <31302181+taikusa@users.noreply.github.com>
2023-11-22 10:11:32 +0900

    daisyuiを追加&設定

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: "pastel", // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
+  },
 }
-
