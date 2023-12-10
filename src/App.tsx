b3a0a058a9322f9e5fe55d852a1f267cb5418372
Taishun Kusaka <31302181+taikusa@users.noreply.github.com>
2023-12-10 23:16:56 +0900

    横マージン幅を大きく

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,8 @@ import { faThumbsUp } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
   return (
-    <div className="flex flex-col gap-10">
-      <div className="flex-none navbar shadow-xl rounded-box bg-base-100 m-4 px-4 w-auto">
+    <div className="flex flex-col gap-16">
+      <div className="flex-none navbar shadow-xl rounded-box bg-base-100 sm:mx-16 mx-8 mt-12 px-4 w-auto">
         <div className="flex-1 flex items-center gap-2">
           <img src={logo} alt="logo" width="40" />
         </div>
@@ -21,12 +21,12 @@ function App() {
       </div>
 
       <div className="flex-2 gap-6">
-        <p className="text-2xl font-extrabold px-6 pb-6">
+        <p className="text-2xl font-extrabold sm:mx-16 mx-8 mb-6">
           🏆 今月の
           <FontAwesomeIcon icon={faThumbsUp} className="text-2xl mx-1" />
           ランキング
         </p>
-        <div className="flex lg:flex-row flex-col gap-6 px-6 pb-8 -mb-4 w-full">
+        <div className="flex lg:flex-row flex-col gap-6 sm:px-16 px-8 pb-8 -mb-4 w-full">
           <RankingCard />
           <RankingCard />
           <RankingCard />
@@ -34,10 +34,13 @@ function App() {
       </div>
 
       <div className="flex-2 gap-6">
-        <p className="text-2xl font-extrabold px-6 pb-6">
-          👀 今作成されたプロンプト
-        </p>
-        <div className="carousel carousel-center gap-4 px-6 pb-8 -mb-4 w-full">
+        <div className="flex flex-row">
+          <p className="text-2xl font-extrabold sm:mx-16 mx-8 mb-6">
+            👀 作成されたプロンプト
+          </p>
+          {/* "今作成された"、ではなく"作成された"にして新着順かを選択するドロップダウンを追加 & 検索欄を追加。どちらもタイトルの右側に表示*/}
+        </div>
+        <div className="carousel carousel-center gap-4 sm:px-16 px-8 pb-8 -mb-4 w-full">
           <div className="carousel-item">
             <PostCard />
           </div>
@@ -60,10 +63,10 @@ function App() {
       </div>
 
       <div className="flex-2">
-        <p className="text-2xl font-extrabold px-6 pb-6">
+        <p className="text-2xl font-extrabold sm:mx-16 mx-8 mb-6">
           ✏️ プロンプトを作成する
         </p>
-        <div className="card bg-base-100 mx-6 mb-6">
+        <div className="card bg-base-100 sm:mx-16 mx-8 mb-12">
           <div className="card-body">
             <PostForm />
           </div>
