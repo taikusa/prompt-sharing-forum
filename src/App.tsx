ee20d531d2180221328ea71ca31e8789462f74df
Taishun Kusaka <31302181+taikusa@users.noreply.github.com>
2023-12-10 23:34:12 +0900

    カードのshadow表示のためマージン幅を小さく

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ function App() {
           <FontAwesomeIcon icon={faThumbsUp} className="text-2xl mx-1" />
           ランキング
         </p>
-        <div className="flex sm:flex-row flex-col gap-6 sm:px-16 px-8 pb-8 -mb-4 w-full">
+        <div className="flex sm:flex-row flex-col gap-6 sm:px-16 px-8 pb-8 -mb-8 w-full">
           <RankingCard />
           <RankingCard />
           <RankingCard />
@@ -40,7 +40,7 @@ function App() {
           </p>
           {/* "今作成された"、ではなく"作成された"にして新着順かを選択するドロップダウンを追加 & 検索欄を追加。どちらもタイトルの右側に表示*/}
         </div>
-        <div className="carousel carousel-center gap-4 sm:px-16 px-8 pb-8 -mb-4 w-full">
+        <div className="carousel carousel-center gap-4 sm:px-16 px-8 pb-8 -mb-8 w-full">
           <div className="carousel-item">
             <PostCard />
           </div>
